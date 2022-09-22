e730aab277807a13c618caef357f4136b90b1065
PawelWP <pawelwolak13@gmail.com>
2022-09-22 17:11:54 +0200

    test

--- firsttestCi.test.js
+++ firsttestCi.test.js
@@ -1,9 +1,10 @@
 const mocha = require('mocha')
-const should = reguire('chai')
+const chai = require('chai')
+const { expect } = require('chai')
 describe("Ci testing", () => {
     it("test #1", () => {
 
         var value = 5
-        value.should.equal(5)
+        expect(value).to.equal(5)
     })
 })
